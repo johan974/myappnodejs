5c9f16abaedbaea97bf92bebf6d963a72447f0b0
Dick Holtman <info@lightroomstatistics.com>
2016-08-17 10:43:05 +0200

    New line \n replace by </br> since we are using html

--- users_rest_server.js
+++ users_rest_server.js
@@ -10,5 +10,5 @@ http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello Johan Holtman. Leuk dat je even komt kijken.!\r\n✔ Express server listening at ' + app.get('ip') + ', server port ' + app.get('port'));
+  res.send('Hello Johan Holtman. Leuk dat je even komt kijken.!</br></br>✔ Express server listening at ' + app.get('ip') + ', server port ' + app.get('port'));
 });
